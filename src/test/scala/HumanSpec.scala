5f37c1eaae5d5c2216d045ca253d0aa9e03aa8d7
Clément Roig <clm.roig@gmail.com>
2018-10-01 12:29:20 +0200

    HumanSpec : more test

--- src/test/scala/HumanSpec.scala
+++ src/test/scala/HumanSpec.scala
@@ -2,6 +2,20 @@ package battleship
 import org.scalatest.FunSuite
 
 class HumanSpec extends FunSuite {
+
+    // ====== askToPlaceAShip() tests
+    test("askToPlaceAShip(): common usage.") {
+        val inputString = new MockConsoleInput(List("5", "a", "E"))
+        val human = Human("Kévin", input = inputString) 
+        val newHuman = human.askToPlaceAShip(new Ship("Destroyer", "D", 2))
+        assert(newHuman match {
+            case g: Human => true
+            case _ => false
+        })
+        assert(newHuman.myGrid.positions(5)(0) == "D")
+        assert(newHuman.myGrid.positions(5)(1) == "D")
+    }
+
     // ====== askToEnterXCoordinate() tests
     test("askToEnterXCoordinate(): common usage.") {
         val inputString = new MockConsoleInput(List("5"))
@@ -9,7 +23,15 @@ class HumanSpec extends FunSuite {
         assert(human.askToEnterXCoordinate() == 5)
     }
 
-    test("askToEnterXCoordinate(): x too big or to small.") {
+    test("askToEnterXCoordinate(): invalid x (char).") {
+        // askToEnterXCoordinate() will be call 2 times because z is not a valid value.
+        // Finally, it will return 3 which is a valid value
+        val inputString = new MockConsoleInput(List("z", "3"))
+        val human = Human("Kévin", input = inputString)   
+        assert(human.askToEnterXCoordinate() == 3)
+    }
+
+    test("askToEnterXCoordinate(): invalid x (too big or to small).") {
         // askToEnterXCoordinate() will be call 3 times because 23 and -1 are not valid values.
         // Finally, it will return 3 which is a valid value
         val inputString = new MockConsoleInput(List("23", "-1", "3"))
@@ -31,5 +53,18 @@ class HumanSpec extends FunSuite {
         val inputString = new MockConsoleInput(List("23", "z", "C"))
         val human = Human("Kévin", input = inputString)   
         assert(human.askToEnterYCoordinate() == 'C')
+    }
+
+    // ====== askToEnterDirection() tests
+    test("askToEnterDirection(): common usage.") {
+        val inputString = new MockConsoleInput(List("E"))
+        val human = Human("Kévin", input = inputString)   
+        assert(human.askToEnterDirection() == "E")
+    }
+
+    test("askToEnterDirection(): invalid direction.") {
+        val inputString = new MockConsoleInput(List("X", "N"))
+        val human = Human("Kévin", input = inputString)   
+        assert(human.askToEnterDirection() == "N")
     }
 }
